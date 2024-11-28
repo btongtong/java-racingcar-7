86bf5fa49518bb59c116e2b27c91abec5710e5cb
btongtong <leeym2615@naver.com>
2024-11-29 02:55:29 +0900

    feat: Add method to validate duplication name

--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -1,9 +1,21 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validation {
     public static void validateNameFormat(String input) {
         String regex = "([^\\s]{1,5},)*[^\\s]{1,5}";
         if (!input.matches(regex)) {
+            throw new IllegalArgumentException(ErrorMessage.NAME_FORMAT_ERROR.getMessage());
+        }
+    }
+
+    public static void validateDuplicationName(String input) {
+        List<String> names = Arrays.asList(input.split(","));
+        if (names.size() != names.stream().distinct().count()) {
             throw new IllegalArgumentException(ErrorMessage.NAME_FORMAT_ERROR.getMessage());
         }
     }
