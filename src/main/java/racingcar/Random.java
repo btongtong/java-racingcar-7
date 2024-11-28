02d720e146a90f22c28e06595e9868613432d5e2
btongtong <leeym2615@naver.com>
2024-11-29 01:56:10 +0900

    style: Change constant variable to capital letter name

--- src/main/java/racingcar/Random.java
+++ src/main/java/racingcar/Random.java
@@ -3,11 +3,11 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Random {
-    private static final int startNum = 0;
-    private static final int endNum = 9;
+    private static final int START = 0;
+    private static final int END = 9;
 
     private static int getRandomNumber() {
-        return Randoms.pickNumberInRange(startNum, endNum);
+        return Randoms.pickNumberInRange(START, END);
     }
 
     public static boolean isParamOrMore(int randomNumber, int criteria) {
