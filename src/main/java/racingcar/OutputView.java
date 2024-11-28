4fe1457922058282d4a85588607d8c9dca136ccd
btongtong <leeym2615@naver.com>
2024-11-29 03:35:17 +0900

    fix: Separate the title from the result

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -3,9 +3,12 @@ package racingcar;
 import java.util.List;
 
 public class OutputView {
-    public void printResult(List<Car> cars) {
+    public void printResultTitle() {
         System.out.println(IOMessage.RESULT_TITLE.getMessage());
+    }
+    public void printResult(List<Car> cars) {
         cars.forEach(car -> System.out.println(IOMessage.RESULT_CONTENT.getMessage(car.getName(), car.getMove())));
+        System.out.println();
     }
 
     public void printFinalWinner(List<String> cars) {
