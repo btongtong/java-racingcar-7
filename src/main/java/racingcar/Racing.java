425917ac1cabfee71f44e94d451cbbb5e70c1f75
btongtong <leeym2615@naver.com>
2024-11-29 02:56:22 +0900

    feat: Add racing class to handle car list

---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Racing {
+    private static final int moveCriteria = 4;
+    List<Car> cars;
+
+    public Racing(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void moveCars() {
+        cars.stream()
+                .filter(car -> Random.isParamOrMore(Random.getRandomNumber(), moveCriteria))
+                .forEach(Car::move);
+    }
+
+    public int getMaxMove() {
+        return cars.stream().mapToInt(car -> car.getMove().length()).max().orElse(0);
+    }
+
+    public List<Car> getWinners() {
+        int maxMove = getMaxMove();
+        return cars.stream().filter(car -> car.getMove().length() == maxMove).collect(Collectors.toList());
+    }
+}
