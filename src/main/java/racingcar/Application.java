f3778d8e903ed49d5fcc5a0a39baa4c6b523fe5f
btongtong <leeym2615@naver.com>
2024-11-29 03:40:40 +0900

    feat: Asemble each objects and complete the logic

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,42 @@
 package racingcar;
 
 public class Application {
+    private static final InputView inputView;
+    private static final OutputView outputView;
+
+    static {
+        inputView = new InputView();
+        outputView = new OutputView();
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        run();
+    }
+
+    private static void run() {
+        Racing racing = new Racing(getName(inputView.receiveCarName()));
+        int conductCount = getConductCount(inputView.receiveConductCount());
+        race(conductCount, racing);
+        outputView.printFinalWinner(racing.getWinners());
+    }
+
+    private static String getName(String input) {
+        Validation.validateNameFormat(input);
+        Validation.validateDuplicationName(input);
+        return input;
+    }
+
+    private static int getConductCount(String input) {
+        Validation.validateConductCountFormat(input);
+        Validation.validateNumberFormat(input);
+        return Integer.parseInt(input);
+    }
+
+    private static void race(int conductCount, Racing racing) {
+        outputView.printResultTitle();
+        while(conductCount-- > 0) {
+            racing.moveCars();
+            outputView.printResult(racing.getCars());
+        }
     }
 }
